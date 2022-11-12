e977f1a29d7393d4656b6005bdae701ce4f07de4
ReemAlk <r.yk99@hotmail.com>
2022-11-13 02:04:20 +0300

    first commit

---
+++ RowbinView.swift
@@ -0,0 +1,17 @@
+//
+//  RowbinView.swift
+//  Rowbin
+//
+//  Created by Reem Alkhatib on 13/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct RowbinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Loading_page()
+        }
+    }
+}
